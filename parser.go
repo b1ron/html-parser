c6688e5be857f5236270d51d982ada11b45f9732
b1ron <goldingbyron@gmail.com>
2024-09-17 23:49:30 +0200

    rename func

--- parser.go
+++ parser.go
@@ -163,7 +163,7 @@ func (s *scanner) scanIdent() (lit string) {
 		if ch == EOF {
 			break
 		}
-		if isState(ch) {
+		if isAction(ch) {
 			s.unread()
 			break
 		}
@@ -172,8 +172,8 @@ func (s *scanner) scanIdent() (lit string) {
 	return
 }
 
-// isState returns true for tokens corresponding to state transitions or parsing actions
-func isState(ch rune) bool {
+// isAction returns true for tokens corresponding to state transitions or parsing actions
+func isAction(ch rune) bool {
 	return ch == '<' || ch == '>' || ch == '/' || ch == '&' || ch == '!' || ch == ' '
 }
 
