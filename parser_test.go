922d9e930c5ac120290e0f52723c1dfa872cbd88
b1ron <goldingbyron@gmail.com>
2024-09-18 22:09:14 +0200

    more test cases

--- parser_test.go
+++ parser_test.go
@@ -11,10 +11,63 @@ var simple = `
   </html>
 `
 
+var simple2 = `
+<!DOCTYPE html>
+  <html>
+    <head>
+	  <title>Test</title>
+	</head>
+	<body>
+	  <h1>Test</h1>
+	</body>
+  </html>
+`
+
+var withAttributes = `
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+	  <title>Test</title>
+	</head>
+	<body>
+	  <h1>Test</h1>
+	</body>
+  </html>
+`
+
+var withLinks = `
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+	  <title>Test</title>
+	  <link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+	  <h1>Test</h1>
+	</body>
+  </html>
+`
+
 func TestParser(t *testing.T) {
-	p := newParser(strings.NewReader(simple))
-	tr := p.parse()
-	for _, n := range tr.root.children {
-		t.Logf("node type: %s, node data: %s", n.Type(), n.Data())
+	tests := []struct {
+		name string
+		in   string
+	}{
+		{"simple", simple},
+		{"simple2", simple2},
+		{"withAttributes", withAttributes},
+		{"withLinks", withLinks},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := newParser(strings.NewReader(tt.in))
+			tr := p.parse()
+			for _, child := range tr.root.children {
+				if child.Type() == "element" {
+					t.Logf("Element: %s", child.Data())
+				}
+			}
+		})
 	}
 }
